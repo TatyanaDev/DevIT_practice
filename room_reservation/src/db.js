80a41883c0ee995f22204a7ba5f29a52d0624e59
Tatyana404 <kot.kate2020@gmail.com>
2024-01-25 16:30:49 +0100

    delete room_reservation

--- room_reservation/src/db.js
+++
@@ -1,9 +0,0 @@
-const fakeDatabase = {
-  rooms: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }],
-  users: [],
-  reservations: [{ id: Date.now(), room_id: '', user_id: '', from: '', to: '' }]
-}
-
-module.exports = {
-  fakeDatabase
-}
--- room_reservation/src/models/Room.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-const ObjectId = Schema.ObjectId
-
-export const Room = mongoose.model('Room', { id: ObjectId })
